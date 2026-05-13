256783ce92512ee39154a208d5073929278d721e
jonas aerts <jonas.aerts@hotmail.com>
2026-05-13 17:35:21 +0200

    new

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Configuration
+MAIN_REPO="git@github.com:JonasAerts/TheaterStap_odoo.git"
+ADDONS_REPO="git@github.com:JonasAerts/theaterstap_odoo_addons.git"
+PROJECT_DIR="/opt/TheaterStapOdoo"
+
+echo "--- Starting Deployment Pipeline ---"
+
+# Navigate to project directory
+cd $PROJECT_DIR || { echo "Directory $PROJECT_DIR not found"; exit 1; }
+
+# Update main repository
+echo "Updating main repository..."
+git pull origin main
+
+# Update or Clone addons repository
+if [ -d "addons/.git" ]; then
+    echo "Updating addons repository..."
+    cd addons
+    git pull origin main
+    cd ..
+else
+    echo "Cloning addons repository..."
+    rm -rf addons
+    git clone $ADDONS_REPO addons
+fi
+
+# Build and Restart Docker Containers
+echo "Rebuilding and restarting Docker containers..."
+docker-compose up -d --build
+
+# Clean up unused images to save space on NAS
+echo "Cleaning up unused Docker images..."
+docker image prune -f
+
+echo "--- Deployment Complete ---"
+echo "Odoo should be available at http://localhost:8069"
--- dockerfile
+++
@@ -1,5 +0,0 @@
-FROM odoo:18
-USER root
-RUN apt-get update && apt-get upgrade -y
-WORKDIR /opt/odoo/parts/custom
-COPY ./addons .
